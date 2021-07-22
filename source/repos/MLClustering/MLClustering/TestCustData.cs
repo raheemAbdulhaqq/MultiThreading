a125da7af7cfceba73eb4d16d85516a35c650c43
Raheem Abdulhaqq <raheemabdulhaqq2020@gmail.com>
2021-07-22 10:42:43 +0100

    First Commit

---
+++ source/repos/MLClustering/MLClustering/TestCustData.cs
@@ -0,0 +1,13 @@
+﻿namespace MLClustering
+{
+    static class TestCustData
+    {
+        internal static readonly CustData PredictionObj = new CustData
+        {
+            Male = 300f,
+            Female = 100f,
+            Before2010 = 400f,
+            After2010 = 1400f
+        };
+    }
+}
