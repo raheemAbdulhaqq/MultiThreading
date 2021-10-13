e29cafe9a47c6d73aad12f513e23dae8261d535f
Raheem Abdulhaqq <raheemabdulhaqq2020@gmail.com>
2021-10-13 19:41:08 +0100

    Initial Commit

---
+++ source/repos/RESTSharp.Consume/RESTSharp.Consume/Program.cs
@@ -0,0 +1,23 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+
+namespace RESTSharp.Consume
+{
+    class Program
+    {
+        private static RestClient client = new
+        RestClient("http://localhost:45767/api/");
+        static void Main(string[] args)
+        {
+            RestRequest request = new RestRequest("Default", Method.GET);
+            IRestResponse<List<string>> response = client.Execute<List<string>>(request);
+            Console.WriteLine("{0}", response.Data);
+            Console.ReadKey();
+
+            //RestRequest request = new RestRequest("Default", Method.POST);
+            //request.AddJsonBody("Robert Michael");
+            //var response = client.Execute(request);
+        }
+    }
+}
